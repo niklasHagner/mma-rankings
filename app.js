3a1ec7d0ac7ec9bdc71317b7985f1f0c05428e70
niklasHagner <niklas.hagner@outlook.com>
2022-05-08 14:10:36 +0200

    Nunjucks rendering

--- app.js
+++ app.js
@@ -1,95 +1,123 @@
 const express = require("express");
 const app = express();
+const nunjucks = require("nunjucks");
 const fs = require('fs');
 const path = require('path');
 const winston = require('winston');
 const mmaStatsScraper = require('./backend/scrapeMmaStatsDotCom');
 const { findRankAtTime } = require('./backend/findRank');
 const wikipediaApi = require('./backend/wikipediaApi.js');
+const viewBuilder = require('./backend/viewBuilder.js');
 
 let existingData = fs.readFileSync("data/mmaStats.json");
 let jsonData = JSON.parse(existingData);
 let lastScapedDate = jsonData.dates[0].date;
 console.log(`The last rank is dated ${lastScapedDate}. Maybe it's time to run /scrapeMissing `);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// parseFighter('Nick Diaz')
+app.use(express.static('public'));
+
+app.set("view engine", ".njk");
+
+nunjucks.configure("views", {
+  autoescape: true,
+  trimBlocks: true,
+  lstripBlocks: true,
+  express: app
+})
+  .addFilter("classList", (classList) => classList.filter((x) => x).join(" "))
+  .addFilter("inlineFileContent", (pathToFile) => {
+    const fileContent = fs.readFileSync(pathToFile).toString();
+    if (pathToFile.endsWith(".js")) {
+      return fileContent.replace("sourceMappingURL=", "sourceMappingURL=/scripts/");
+    }
+    return fileContent;
+  })
 
 winston.configure({
-    transports: [
-        new (winston.transports.File)({ filename: 'app.log' })
-    ]
+  transports: [
+    new (winston.transports.File)({ filename: 'app.log' })
+  ]
 })
 
 var settings = {
-    logOutputOnce: true,
-    loggedResponses: 0,
-    caching: false,
-    storedResponses: {}, //keymap
+  logOutputOnce: true,
+  loggedResponses: 0,
+  caching: false,
+  storedResponses: {}, //keymap
 }
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
+app.get('/', async function (req, res, next) {
+  const rankData = await getRankingsFromFile(req, res);
+  const rankingsHtmlString = viewBuilder.buildRankingsHtml(rankData.dates);
+  const viewModel = {
+    rankings: {
+      dates: rankData.dates,
+      htmlString: rankingsHtmlString 
+    }
+  };
+  res.render("base.njk", viewModel, (err, html) => {
+    if (err) return next(err);
+    res.send(html);
+  });
 });
 
 const defaultResponse = {
-    error: true
+  error: true
 };
 
 app.get('/searchfileforfighter', function (req, res) {
-    const name = req.query.name || "Jon Jones";
-    const date = req.query.date || "2016-01-02";
-    
-    let rawdata = fs.readFileSync("data/mmaStats.json");
-    let data = JSON.parse(rawdata);
-    const rank = findRankAtTime(data, name, date);
-    const jsonResult = { "mostRecentMatch": rank };
-    return res.json(jsonResult);
+  const name = req.query.name || "Jon Jones";
+  const date = req.query.date || "2016-01-02";
+
+  let rawdata = fs.readFileSync("data/mmaStats.json");
+  let data = JSON.parse(rawdata);
+  const rank = findRankAtTime(data, name, date);
+  const jsonResult = { "mostRecentMatch": rank };
+  return res.json(jsonResult);
 });
 
 app.get('/scrapeMissing', async function (req, res) {
-    let existingData = fs.readFileSync("data/mmaStats.json");
-    let jsonData = JSON.parse(existingData);
-    let lastScapedJsonBlob = jsonData.dates[0];
-    const startDate = lastScapedJsonBlob.date;
-    const today = new Date().toISOString().split('T')[0];
-    const endDate = today;
-    const scrapeStatus = await mmaStatsScraper.scrapeRankingsForMultipleDates(startDate, endDate);
-    res.send(scrapeStatus);
+  let existingData = fs.readFileSync("data/mmaStats.json");
+  let jsonData = JSON.parse(existingData);
+  let lastScapedJsonBlob = jsonData.dates[0];
+  const startDate = lastScapedJsonBlob.date;
+  const today = new Date().toISOString().split('T')[0];
+  const endDate = today;
+  const scrapeStatus = await mmaStatsScraper.scrapeRankingsForMultipleDates(startDate, endDate);
+  res.send(scrapeStatus);
 });
 
 app.get('/scrapeByQueryParams', async function (req, res) {
-    const startDate = req.query.startDate;
-    const endDate = req.query.endDate;
-    const scrapeStatus = await mmaStatsScraper.scrapeRankingsForMultipleDates(startDate, endDate);
-    res.send(scrapeStatus);
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+  const scrapeStatus = await mmaStatsScraper.scrapeRankingsForMultipleDates(startDate, endDate);
+  res.send(scrapeStatus);
 });
 
-app.get('/serve-rankings-file', async function (req, res) {
-    let rawdata = fs.readFileSync("data/mmaStats.json");
-    let jsonData = JSON.parse(rawdata);
-    res.json(jsonData);
-});
+async function getRankingsFromFile(req, res) {
+  let rawdata = fs.readFileSync("data/mmaStats.json");
+  let jsonData = JSON.parse(rawdata);
+  return jsonData;
+}
 
 app.get('/mma-stats-by-date', async function (req, res) {
-    res.contentType('application/json');
-    if (typeof req.query.date === 'undefined') {
-        const errorMessage =" Error. Try something like this instead: /mma-stats-by-date?date=2019-02-30";
-        console.error(errorMessage);
-        res.send({error:errorMessage});
-        return;
-    }
-    console.log('called with query:', req.query);
+  res.contentType('application/json');
+  if (typeof req.query.date === 'undefined') {
+    const errorMessage = " Error. Try something like this instead: /mma-stats-by-date?date=2019-02-30";
+    console.error(errorMessage);
+    res.send({ error: errorMessage });
+    return;
+  }
+  console.log('called with query:', req.query);
 
-    const json = await mmaStatsScraper.scrapeMmaStats(req.query.date);
-    res.send(json);
+  const json = await mmaStatsScraper.scrapeMmaStats(req.query.date);
+  res.send(json);
 });
 
 // app.get('/search-fighter-by-name', function(req, response) {
@@ -118,36 +146,36 @@ app.get('/mma-stats-by-date', async function (req, res) {
 // });
 
 app.get('/fighters-from-next-events', async function (req, response) {
-    let allRankingsFromFile = fs.readFileSync("data/mmaStats.json");
-    let allRankingsData = JSON.parse(allRankingsFromFile);
-    const data = await wikipediaApi.getInfoAndFightersFromNextEvents(); // { events: [] }
-    //Extend fighter-objects with historical ranking data
-    const events = data.events.map(event => {
-        let fighters = event.fighters.map((fighter) => { 
-            return mapFighterFromApiToExtraData(fighter, allRankingsData); 
-        });
-        return {...event, fighters};
-    })
-    response.contentType('application/json');
-    response.send({allEvents: events});
-    return;
+  let allRankingsFromFile = fs.readFileSync("data/mmaStats.json");
+  let allRankingsData = JSON.parse(allRankingsFromFile);
+  const data = await wikipediaApi.getInfoAndFightersFromNextEvents(); // { events: [] }
+  //Extend fighter-objects with historical ranking data
+  const events = data.events.map(event => {
+    let fighters = event.fighters.map((fighter) => {
+      return mapFighterFromApiToExtraData(fighter, allRankingsData);
+    });
+    return { ...event, fighters };
+  })
+  response.contentType('application/json');
+  response.send({ allEvents: events });
+  return;
 });
 
 function mapFighterFromApiToExtraData(fighter, allRankingsData) {
-    const record = fighter.record;
-    const extendedRecord = record.map((fight) => {
-        const lookupName = fight.opponentName;
-        const lookupDate = fight.date;
-        const opponentInfoAtTheTime = findRankAtTime(allRankingsData, lookupName, lookupDate);
-        return { ...fight, opponentInfoAtTheTime };
-    });
-    fighter.record = extendedRecord;
-    return fighter;
+  const record = fighter.record;
+  const extendedRecord = record.map((fight) => {
+    const lookupName = fight.opponentName;
+    const lookupDate = fight.date;
+    const opponentInfoAtTheTime = findRankAtTime(allRankingsData, lookupName, lookupDate);
+    return { ...fight, opponentInfoAtTheTime };
+  });
+  fighter.record = extendedRecord;
+  return fighter;
 }
 
 var port = 8081;
 console.log('Server listening on:' + port);
 app.listen(port);
-console.info("* Endpoint example: /search-fighter-by-name?name=Fedor");
-console.info("* to trigger a request browse to /");
-console.info("* to scrape go to /scrape?startDate=2017-01-01&endDate=2017-12-31");
+console.info(`Navigate to http://localhost:${port}/`);
+console.info("...To scrape data navigate to http://localhost:${port}/scrapeMissing or http://localhost:${port}/scrape?startDate=2017-01-01&endDate=2017-12-31");
+// console.info("* Endpoint example: /search-fighter-by-name?name=Fedor");
--- public/main.js
+++ public/main.js
@@ -93,115 +93,6 @@ const buildAllFightersHtml = function(fighters) {
   return allHtml;
 };
 
-const divisionToHtml = function(division) {
-
-  const fighters = division.fighters.map((fighter) => {
-    if (fighter.rank.toLowerCase().indexOf("champion") > -1) 
-      fighter.rank = "C";
-
-    return `
-      <article>
-        <span class="rank">${fighter.rank}</span> <span class="name">${fighter.name}</span>
-      </article>
-    `;
-  }).join("<br>");
-  return `
-    <article class="division">
-        <h3 class="division-name">${division.name}</h3>
-        <div class="division-fighters">${fighters}</div>
-    </article>
-  `;
-};
-
-const getSortedDivisions = function(divisions) {
-  const sortValues = [
-    {name: "heavyweight", order: 1},
-    {name: "light heavyweight", order: 2},
-    {name: "middleweight", order: 3},
-    {name: "welterweight", order: 4},
-    {name: "lightweight", order: 5},
-    {name: "featherweight", order: 6},
-    {name: "bantamweight", order: 7},
-    {name: "flyweight", order: 8},
-    {name: "women's bantamweight", order: 9},
-    {name: "women's flyweight", order: 10},
-    {name: "women's strawweight", order: 11},
-    {name: "women's featherweight", order: 12},
-    {name: "pound-for-pound", order: 13},
-  ];
-  const arr = divisions.filter(x => x.name.indexOf("women") < 0);
-  return arr.sort((a,b) => {
-    const aMatch = sortValues.find(sortItem => sortItem.name === a.name.toLowerCase().trim());
-    const aSortVal = aMatch ? aMatch.order : 0;
-
-    const bMatch = sortValues.find(sortItem => sortItem.name === b.name.toLowerCase().trim());
-    const bSortVal = bMatch ? bMatch.order : 0;
-    
-    return aSortVal - bSortVal; 
-  });
-}
-
-const buildRankingsHtml = function(pages) {
-  pages.forEach((x) => {
-    x.divisions = getSortedDivisions(x.divisions);
-  });
-  let latestRanks = pages[0];
-  pages = pages.slice(0,pages.length-1);
-  const snapshots = pages.map((page) => {
-    
-    const divisionTemplates = page.divisions
-      .filter(currDivision => currDivision.fighters.length >= 10)
-      .map(division => divisionToHtml(division));
-
-    const snapshotHtml = ` 
-      <section class="snapshot">
-        <h2>Rankings ${page.date}</h2>
-        <div class="snapshot-divisions">
-            ${divisionTemplates.join("")}
-        </div>
-      </section>
-    `;
-    return { 
-      date: page.date, 
-      year: new Date(page.date).getFullYear(), 
-      html: snapshotHtml
-    };
-  });
-
-  const snapshotsGroupedByYear = [];
-  snapshots.forEach((snapshot) => {
-    const matchingItem = snapshotsGroupedByYear.find(x => x.year === snapshot.year);
-    if (matchingItem) {
-      const matchingIndex = snapshotsGroupedByYear.indexOf(matchingItem);
-      snapshotsGroupedByYear[matchingIndex].html += snapshot.html;
-    } else {
-      snapshotsGroupedByYear.push(snapshot);
-    }
-  }) 
-
-  let totalHtmlString = snapshotsGroupedByYear.map((annualSnapshot, index) => {
-    // const modifierClass = index === 0 ? " : "annual-rank-snapshots--collapsed";
-    const modifierClass = "annual-rank-snapshots--collapsed";
-    return `
-      <div class="annual-rank-snapshots ${modifierClass}">
-        <p>${annualSnapshot.year}</p>
-        ${annualSnapshot.html}
-      </div>
-    `;
-  }).join("");
-
-  totalHtmlString = `
-      <h2>Rankings ${latestRanks.date}</h2>
-      <section class="snapshot-divisions">
-        ${latestRanks.divisions.map(x => divisionToHtml(x)).join("")}
-      </section>
-      <h1 class="post-title">UFC rankings history</h1>
-      ${totalHtmlString}
-    </section>
-  `;
-  return totalHtmlString;
-};
-
 const searchByName = function(ev) {
   ev.preventDefault();
   var name = document.querySelector("#search-input").value;
@@ -308,22 +199,6 @@ function formatDate(date) {
 //     });
 // };
 
-window.getHistoricalRankingsFromJsonFile = function (date = new Date()) {
-    date = formatDate(date);
-    loader.classList.remove("hidden");
-    fetch(`http://localhost:8081/serve-rankings-file`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        loader.classList.add("hidden");
-        document.querySelector("#rankings").innerHTML = `
-          ${buildRankingsHtml(json.dates)}
-        `;
-        Array.from(document.querySelectorAll(".annual-rank-snapshots p")).forEach(x => x.addEventListener("click", clickAnnualRankingsSnapshot));
-      });
-};
-
 function clickFighterLink(url) {
   renderFighterProfileByUrl(url);
 }
@@ -343,5 +218,5 @@ document.addEventListener('DOMContentLoaded', (e) => {
     searchForm.addEventListener("submit", searchByName);
   }
 
-  window.getHistoricalRankingsFromJsonFile();
+  Array.from(document.querySelectorAll(".annual-rank-snapshots p")).forEach(x => x.addEventListener("click", clickAnnualRankingsSnapshot));
 });
