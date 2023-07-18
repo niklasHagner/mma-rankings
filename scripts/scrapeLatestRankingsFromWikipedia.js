07ff5dcdfa775de4d1d0b16ac3aadd823831b3d6
niklasHagner <niklas.hagner@outlook.com>
2023-07-18 22:32:48 +0200

    mma-stats.com/rankings is dead. Use wikipedia

--- scripts/scrapeLatestRankingsFromWikipedia.js
+++ scripts/scrapeLatestRankingsFromWikipedia.js
@@ -1,6 +1,32 @@
 const fs = require('fs');
 const moment = require("moment");
 const wikipediaRankScraper = require('../backend/scrapeWikipediaRankings.js');
+const { uniqueBy } = require("../backend/arrHelper");
+
+/*
+    EXAMPLE DATA:
+    newRankDateObj example: {date: 'May 16, 2023', divisions: Array(12)}
+
+    REQUIREMENTS:
+    The date-string must incoude year
+*/
+function saveToFile(newRankDateObj) {
+    const fileName = "data/mmaStats.json";
+    let rawdata = fs.readFileSync(fileName);
+    let data = JSON.parse(rawdata);
+    if (data && data.dates) {
+      console.log("Reading", fileName, "\nIt contains", data.dates.length);
+    }
+  
+    const concatted = data.dates.concat(newRankDateObj);
+    const sorted = concatted.sort((a,b) => new Date(b.date) - new Date(a.date));
+    const unique = uniqueBy(sorted, "date");
+    data.dates = unique;
+    console.log("File now contains", data.dates.length, "dates:", data.dates.map(x => x.date).join(", "));
+    fs.writeFileSync(fileName, JSON.stringify(data));
+    console.log (`File ${fileName} updated successfully`);
+    return { scrapedUrlCount: newRankDateObj.length };
+  }
 
 async function scrapeLatestRankings() {
   let existingData = fs.readFileSync("data/mmaStats.json");
@@ -24,8 +50,9 @@ async function scrapeLatestRankings() {
     startDate = today;
   }
 
-  const scrapeStatus = await wikipediaRankScraper.scrapeRankings(startDate, today);
-  return scrapeStatus;
+  //Example: { date: '', data: |]}
+  const scrapedData = await wikipediaRankScraper.scrapeRankings(startDate, today);
+  saveToFile(scrapedData);
 }
 
 scrapeLatestRankings();
